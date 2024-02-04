2637a5695be1ab83cbb9022bfc791b4e0bd4a972
chieftain0 <bexruzerk3210@gmail.com>
2024-02-04 17:18:56 +0400

    Added engine sound

---
+++ Assets/Scripts/EngineSoundControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EngineSoundControl : MonoBehaviour
+{
+    public string MasterObjectName = "F16";
+    public AudioSource audioSource = null;
+    public float volumeIncreaseSpeed = 3.0f;
+    AirplaneController airplaneController;
+    GameObject aircraft;
+
+    void Start()
+    {
+        aircraft = GameObject.Find(MasterObjectName);
+        airplaneController = aircraft.GetComponent<AirplaneController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(airplaneController.engineState == 0f)
+        {
+            audioSource.volume = 0f;
+        }
+        else
+        {
+            //audioSource.volume = Mathf.Clamp(airplaneController.thrustPercent, 0.4f, 1f);
+            float targetVolume = Mathf.Clamp(airplaneController.thrustPercent, 0.4f, 1f);
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, targetVolume, volumeIncreaseSpeed * Time.deltaTime);
+        }
+    }
+}
